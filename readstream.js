4de95423d073477bedb6ce56c8cfb667f00963bd
RubelMehmed <rubel.49@geography-juniv.edu.bd>
2023-02-16 17:05:39 +0600

    added readstream

--- readstream.js
+++ readstream.js
@@ -1,6 +1,13 @@
 const http = require('http');
 const fs = require('fs');
 
+const neoReadStream = fs.createReadStream(`${__dirname}/bigdata.txt`);
+const neoWriteStream = fs.createWriteStream(`${__dirname}/output.txt`);
+
+neoReadStream.on('data', (chunk) => {
+    neoWriteStream.write(chunk);
+});
+
 const server = http.createServer((req, res) => {
     const myReadStream = fs.createReadStream(`${__dirname}/bigdata.txt`, 'utf-8');
     myReadStream.pipe(res);
---
+++ serve.js
@@ -0,0 +1,31 @@
+p = require('http');
+
+const server = http.createServer((req,res) => {
+    if(req.url === '/') {
+        res.write('<html> Form </html>');
+        res.write(<body> <form method="post" action="/process" > <input name="message" ></input></form></body>);
+        res.end();
+    } else if(req.url === "/process" && method=== "POST"){
+        const body = [];
+        req.on('data', (chunk) => {
+        body.push(chunk);
+    });
+    req.on('end', () => {
+        console.log('stream finished');
+        const parseBody = Buffer.concat(body).toString();
+        console.log(parseBody);
+        res.write('Thank YOu for submiting');
+        res.end();
+
+    });
+    } else {
+
+    };
+server.listen(3000);
+    console.log('listening on post 3000')
+
+
+
+//     git remote add origin https://github.com/RubelMehmed/anode.git
+// git branch -M main
+// git push -u origin main
