c5011ffbeb75c5d5b40916339f788fc1ecbdeb64
Rebel <rubel.49@geography-juniv.edu.bd>
2023-02-16 12:00:12 +0000

    Add new feature

---
+++ server-express.js
@@ -0,0 +1,32 @@
+import bp from 'body-parser';
+// const bp = require('body-parser');
+
+import express from 'express';
+import morgan from 'morgan';
+
+const { urlencoded, json } = bp
+
+const db = {
+  todos: [],
+}
+
+const app = express()
+
+app.use(urlencoded({ extended: true }))
+app.use(json())
+app.use(morgan('dev'))
+
+app.get('/todo', (req, res) => {
+  res.json({ data: db.todos })
+})
+
+app.post('/todo', (req, res) => {
+  const newTodo = { complete: false, id: Date.now(), text: req.body.text }
+  db.todos.push(newTodo)
+
+  res.json({ data: newTodo })
+})
+
+app.listen(8000, () => {
+  console.log('Server on http://localhost:8000')
+})
